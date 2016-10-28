2876061064a2679913295f4564a22acfcc0e892f
Vathsala-M <Vathsala_M@external.mckinsey.com>
2016-10-28 14:19:17 +0530

    Assignment 1:
    Drawing a moving horizontal bar chart. (using divs)

--- public/javascript/visualize.js
+++ public/javascript/visualize.js
@@ -1,26 +1,36 @@
 var randomNumberGenerator = function(n){
 	var randomNumbers = [];
 	for(var i=0;i<n;i++){
-		randomNumbers.push({"value":Math.round(Math.random()*100),"index":i});
+		randomNumbers.push(Math.round(Math.random()*100));
 	}
 	return randomNumbers;
 }
 
 var loadChart = function(){
+	var padding = 20
 	var sampleData = randomNumberGenerator(5);
-	var svg = d3.select(".container")
-	svg.selectAll("p").data(sampleData).enter()
-		.append('p')
-		.text(function(d){return d.value});
-	setTimeout(function(){
-		console.log("------->");
-		console.log("oldData: ",JSON.stringify(sampleData));
+	var container = d3.select(".container")
+	container.selectAll("div").data(sampleData).enter()
+		.append('div')
+		.style('width',function(d){ return (d+padding)+"px"})
+		.style('height','30px')
+		.style('background-color','steelblue')
+		.classed('bars',true)
+		.text(function(d){return d});
+
+    // container.selectAll('div').transition()
+    // 	.duration(5000)
+    // 	.style('width', function(d){return (d*10)+"px"})
+    // 	.text(function(d){return d});
+    // 	// .style("background-color","red");
+
+	setInterval(function(){
 		sampleData.shift();
-		sampleData.push({"value":Math.round(Math.random()*100),"index":5})
-		console.log("newData: ",JSON.stringify(sampleData));
-		svg.selectAll("p").data(sampleData)
-		.text(function(d){return d.value});
-	},5000)
+		sampleData.push(Math.round(Math.random()*100))
+		container.selectAll("div").data(sampleData)
+		.style('width', function(d){return (d+padding)+"px"})
+		.text(function(d){return d});
+	},1000)
 }
 
 window.onload = loadChart;
