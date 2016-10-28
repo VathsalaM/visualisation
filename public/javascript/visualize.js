8f0c815bb81413544ba8bbbcfc0dfbf834f86b38
Vathsala-M <Vathsala_M@external.mckinsey.com>
2016-10-28 17:06:09 +0530

    temp commit

--- public/javascript/visualize.js
+++ public/javascript/visualize.js
@@ -5,32 +5,57 @@ var randomNumberGenerator = function(n){
 	}
 	return randomNumbers;
 }
+var sampleData = randomNumberGenerator(20);
+var padding = 5;
+var isPaused = false;
+
+var colors = [d3.color("blue"),d3.color("skyblue"),d3.color("steelblue")]
+
+console.log(d3.rgb("#F8F8FF"))
 
 var loadChart = function(){
-	var padding = 20
-	var sampleData = randomNumberGenerator(5);
-	var container = d3.select(".container")
-	container.selectAll("div").data(sampleData).enter()
-		.append('div')
-		.style('width',function(d){ return (d+padding)+"px"})
+
+	var h = d3.scaleLinear()
+
+	var c = d3.scaleLinear()
+			.domain([d3.rgb("#F8F8FF"),d3.rgb("#2A52BE")])
+			.range([0,1])
+
+	var container = d3.select(".container");
+
+	var oldDivs = container.selectAll(".bars").data(sampleData,function(d,i,s){console.log(d,i,s[i]+i);return s[i]+i});
+	oldDivs.exit().remove();
+	oldDivs.transition().duration(10).style("top",function(d){return h((sampleData.indexOf(d)*30))+"px"})
+
+	var newdivs = oldDivs.enter().append('div')
+		newdivs.style('width',function(d){ return (d*padding)+"px"})
 		.style('height','30px')
-		.style('background-color','steelblue')
+		.style('background-color',"steelblue")
 		.classed('bars',true)
-		.text(function(d){return d});
+		.style("left", 0)
+		.style("top",function(d){return h((sampleData.indexOf(d)*30))+"px"})
+		.text(function(d){return d})
 
-    // container.selectAll('div').transition()
-    // 	.duration(5000)
-    // 	.style('width', function(d){return (d*10)+"px"})
-    // 	.text(function(d){return d});
-    // 	// .style("background-color","red");
+	newdivs.transition()
+		.duration(10)
+		.style("top",function(d){return h((sampleData.indexOf(d)*30))+"px"})
 
-	setInterval(function(){
-		sampleData.shift();
-		sampleData.push(Math.round(Math.random()*100))
-		container.selectAll("div").data(sampleData)
-		.style('width', function(d){return (d+padding)+"px"})
-		.text(function(d){return d});
-	},1000)
 }
+
+var pause = function(){
+	isPaused = !isPaused;
+}
+
+setInterval(function(){
+	if(isPaused){
+		return;
+	}
+	sampleData.shift();
+	sampleData.push(Math.round(Math.random()*100))
+	loadChart();
+},500)
+
+
+
 
 window.onload = loadChart;
