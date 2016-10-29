02291bf69f9262f783dd0db5d0f54aa5b54497f7
Vathsala-M <Vathsala_M@external.mckinsey.com>
2016-10-29 18:17:08 +0530

    refactor: assignment3

--- public/javascript/studentData.js
+++ public/javascript/studentData.js
@@ -13,35 +13,40 @@ var studentData = [
 	{name:'pokemon',subject:'social studies',score:32}
 ];
 
-var subjectColours = {"maths":"steelblue","english":"darkorange","kannada":"green","science":"red","social studies":"blueviolet","bengali":"brown","tamil":"Violet","sports":"grey"}
+var visualize = function(){
+	var colours = d3.scaleOrdinal()
+					.domain(generateSubjectsList())
+					.range(d3.schemeCategory10)
 
-var visualization = function(){
 	var container = d3.select('.container')
 	var students = container.append('div').classed('students',true);
 	container.append('br');
-	var sortFields = container.append('div').text('Sort By: ').classed('sortFields',true);
+	var sortFields = container.append('div').text('Sort By: ').classed('sortFields',true).classed('label',true);
 	container.append('br');
 	var subjects = container.append('div').classed('subjects',true);
 
 	var studentDiv = students.selectAll('div').data(studentData);
 	studentDiv.enter().append('div')
 		.text(function(d){return d.name+"  "+d.score})
-		.style("background-color",function(d){return subjectColours[d.subject]})
+		.style("background-color",function(d){return colours(d.subject)})
 		.classed("data",true)
 		.style("width",function(d){return (d.score * 3)+"px"});
 	studentDiv.exit().remove();
 
 	var sortDiv = sortFields.selectAll('button').data(generateSortList());
 	sortDiv.enter().append('button')
-	.text(function(d){return d.fieldName})
+	.text(function(d){
+		var fieldName = d.fieldName;
+		return fieldName[0].toUpperCase() + fieldName.slice(1,fieldName.length)
+	})
 	.attr('onclick',function(d){ return "sortBy('"+d.fieldName+"')"});
 
-	subjects.append('p').text('Subject: ');
+	subjects.append('p').text('Subjects: ').classed('subjectDivs',true).classed('label',true);
 	var subjectDiv = subjects.selectAll('div').data(generateSubjectsList());
 	subjectDiv.enter().append('div')
-	.text(function(d){return d.subject})
+	.text(function(d){return d})
 	.classed('subjectDivs',true)
-	.style('background-color',function(d){return d.colour})
+	.style('background-color',function(d){return colours(d)})
 }
 
 var sortBy = function(field){
@@ -53,11 +58,17 @@ var sortBy = function(field){
 }
 
 var generateSubjectsList = function(){
-	return Object.keys(subjectColours).map(function(subject){ return {"subject":subject,"colour":subjectColours[subject]}});
+	var subjects = [];
+	studentData.forEach(function(d){
+		if(subjects.indexOf(d.subject)==-1){
+			subjects.push(d.subject);
+		}
+	})
+	return subjects;
 }
 
 var generateSortList = function(){
 	return Object.keys(studentData[0]).map(function(fieldName){return {"fieldName":fieldName}})
 }
 
-visualization();
+visualize();
